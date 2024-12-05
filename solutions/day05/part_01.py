99486293f1bf60c43fd85506078ea3e0a3d60fc3
Joshua Spratt <jspratt02@icloud.com>
2024-12-05 00:12:01 -0600

    day 5

--- solutions/day05/part_01.py
+++ solutions/day05/part_01.py
@@ -1,5 +1,4 @@
 from src.fetch_input_data import DataRetriever
-from src.input_parser import parse_input_to_matrix
 from src.logger import AdventLogger
 import os
 import time
@@ -7,15 +6,51 @@ import time
 logger = AdventLogger("day05part01").get_logger()
 
 
+def follows_rules(rules, pages):
+    """Determine if a page follows all rules."""
+    position = {num: idx for idx, num in enumerate(pages)}
+
+    for a, b in rules:
+        if a in position and b in position:
+            if position[a] >= position[b]:
+                return False
+    return True
+
+
+def get_page_middle(pages):
+    """Get the middle item from the page"""
+    middle_index = (len(pages) - 1) // 2
+    return pages[middle_index]
+
+
 def solve():
     start_time = time.time()
     logger.info("Starting to solve Day 05, Part 01")
     env = os.environ
     logger.debug("Fetching input data")
-    data_retriever = DataRetriever(2024, 1, env["AOC_COOKIE"])
+    data_retriever = DataRetriever(
+        2024, 5, env["AOC_COOKIE"]
+    )  # TODO: Don't forget to update the day if you copy/paste
     input_data_string = data_retriever.fetch_input_data()
+
+    page_ordering_rules = input_data_string.split("\n\n")[0]
+    rules = [
+        (int(rule.split("|")[0]), int(rule.split("|")[1]))
+        for rule in page_ordering_rules.split("\n")
+    ]
+
+    pages = input_data_string.split("\n\n")[1].strip()
+    pages = [[int(x) for x in page.split(",")] for page in pages.split("\n")]
+
+    middle_page_numbers_sum = 0
+    for page in pages:
+        if follows_rules(rules, page):
+            middle_page_number = get_page_middle(page)
+            middle_page_numbers_sum += middle_page_number
+
+    logger.info(f"Sum of middle page numbers for pages that follow the rules: {middle_page_numbers_sum}")
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     logger.info(f"Solution completed in {elapsed_time:.2f} seconds")
-    return None
+    return middle_page_numbers_sum
--- tests/test_day_05_part_01.py
+++ tests/test_day_05_part_01.py
@@ -3,6 +3,34 @@ from unittest.mock import patch
 from solutions.day05.part_01 import solve
 
 EXAMPLE_INPUT = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
 """
 
 
@@ -10,4 +38,4 @@ EXAMPLE_INPUT = """
 def test_solve(mock_fetch_data):
     mock_fetch_data.return_value = EXAMPLE_INPUT.strip()
     result = solve()
-    assert result == 18
+    assert result == 143
