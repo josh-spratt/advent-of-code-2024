7c78e66c5b3f67a3f6884e420f7d301b63e3a4b0
Joshua Spratt <jspratt02@icloud.com>
2024-12-06 18:46:00 -0600

    day 7 setup

---
+++ solutions/day07/part_01.py
@@ -0,0 +1,23 @@
+from src.fetch_input_data import DataRetriever
+from src.input_parser import parse_input_to_matrix
+from src.logger import AdventLogger
+import os
+import time
+
+logger = AdventLogger("day07part01").get_logger()
+
+
+def solve():
+    start_time = time.time()
+    logger.info("Starting to solve Day 07, Part 01")
+    env = os.environ
+    logger.debug("Fetching input data")
+    data_retriever = DataRetriever(
+        2024, 7, env["AOC_COOKIE"]
+    )  # TODO: Don't forget to update the day if you copy/paste
+    input_data_string = data_retriever.fetch_input_data()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logger.info(f"Solution completed in {elapsed_time:.2f} seconds")
+    return None
