31cca03a2fad78d43d2e509d263e2a504888b9fd
Josh Spratt <jspratt02@icloud.com>
2024-12-06 18:42:11 -0600

    day 6 (#9)

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ import solutions.day04.part_01
 import solutions.day04.part_02
 import solutions.day05.part_01
 import solutions.day05.part_02
+import solutions.day06.part_01
+import solutions.day06.part_02
 from src.logger import AdventLogger
 
 
 def main():
     logger = AdventLogger("all_solutions").get_logger()
     start_time = time.time()
-    solutions.day01.part_01.solve()
-    solutions.day01.part_02.solve()
-    solutions.day02.part_01.solve()
-    solutions.day02.part_02.solve()
-    solutions.day03.part_01.solve()
-    solutions.day03.part_02.solve()
-    solutions.day04.part_01.solve()
-    solutions.day04.part_02.solve()
-    solutions.day05.part_01.solve()
-    solutions.day05.part_02.solve()
+    # solutions.day01.part_01.solve()
+    # solutions.day01.part_02.solve()
+    # solutions.day02.part_01.solve()
+    # solutions.day02.part_02.solve()
+    # solutions.day03.part_01.solve()
+    # solutions.day03.part_02.solve()
+    # solutions.day04.part_01.solve()
+    # solutions.day04.part_02.solve()
+    # solutions.day05.part_01.solve()
+    # solutions.day05.part_02.solve()
+    solutions.day06.part_01.solve()
+    # solutions.day06.part_02.solve()
     end_time = time.time()
     elapsed_time = end_time - start_time
     logger.info(f"All solutions completed in {elapsed_time:.2f} seconds")
